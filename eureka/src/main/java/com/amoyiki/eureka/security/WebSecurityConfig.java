065c71ae1ad63e3c4a61dac604529fd07e9abeab
guoyihui <249586923@qq.com>
2019-04-04 17:56:49 +0800

    init

---
+++ eureka/src/main/java/com/amoyiki/eureka/security/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.amoyiki.eureka.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author amoyiki
+ * @since 2019/4/4
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest()
+                .authenticated().and().httpBasic();
+    }
+}
